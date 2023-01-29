8e8368e54190d9c2984421a95c6811354e784157
pakornsrs <pakorn.srs@gmail.com>
2023-01-29 22:12:57 +0700

    add double linked list

--- DataStructure/DoubleLinkedList.cs
+++ DataStructure/DoubleLinkedList.cs
@@ -19,23 +19,342 @@ namespace Data_Structures.DataStructure
 
         // public params
         public int Length => _length;
-        public DoubleLinkedListNode<T> Head => _head;
-        public DoubleLinkedListNode<T> Tail => _tail;
+        public DoubleLinkedListNode Head => _head;
+        public DoubleLinkedListNode Tail => _tail;
 
         // private params
-        private DoubleLinkedListNode<T>? _head;
-        private DoubleLinkedListNode<T>? _tail;
+        private DoubleLinkedListNode? _head;
+        private DoubleLinkedListNode? _tail;
         private int _length { get; set; }
+
+        // To push a node to Linked link
+        public bool Push(T value)
+        {
+            if (_head == null)
+            {
+                var newNode = new DoubleLinkedListNode(value);
+                _head = newNode;
+                _tail = newNode;
+
+                _length++;
+            }
+            else
+            {
+                var newNode = new DoubleLinkedListNode(value);
+
+                newNode.Previous = _tail;
+                _tail.Next = newNode;
+
+                // Update tail
+                _tail = newNode;
+
+                _length++;
+            }
+
+            return true;
+        }
+
+        // To remove the lase node
+        public bool Pop()
+        {
+            if (_head == null) return false;
+
+            if(_length == 1)
+            {
+                _head = null;
+                _tail = null;
+
+                _length--;
+                return true;
+            }
+            else
+            {
+                var newTail = _tail.Previous;
+                newTail.Next = null;
+
+                _tail = newTail;
+
+                _length--;
+                return true;
+            }
+        }
+
+        // To remove the first (head) node
+        public bool Shift()
+        {
+            if (_head == null) return false;
+
+            if(_length == 1)
+            {
+                _head = null;
+                _tail = null;
+
+                _length--;
+                return true;
+            }
+            else
+            {
+                var newHead = _head.Next;
+                newHead.Previous = null;
+
+                _head = newHead;
+                _length--;
+                return true;
+            }
+        }
+
+        // To add value to the first (head) node 
+        public bool UnShift(T value)
+        {
+            if(_head == null)
+            {
+                var node = new DoubleLinkedListNode(value);
+                _head = node;
+                _tail = node;
+
+                _length++;
+                return true;
+            }
+            else
+            {
+                var node = new DoubleLinkedListNode(value);
+
+                node.Next = _head;
+                _head.Previous = node;
+                _head = node;
+
+                _length++;
+                return true;
+            }
+        }
+
+        // To get a node by index
+        public DoubleLinkedListNode Get(int index)
+        {
+            if (index < 0 || index >= _length) return null;
+
+            var half = Math.Floor((decimal)_length / 2);
+
+            if(index < half)
+            {
+                var current = _head;
+                var count = 0;
+
+                while(count < index)
+                {
+                    var temp = current;
+                    current = temp.Next;
+
+                    count++;
+                }
+
+                return current;
+            }
+            else
+            {
+                var current = _tail;
+                var count = _length - 1;
+
+                while (count > index)
+                {
+                    var temp = current;
+                    current = temp.Previous;
+
+                    count--;
+                }
+
+                return current;
+            }
+        }
+
+        // To set value of selected node
+        public bool Set(int index, T value)
+        {
+            var selectedNode = Get(index);
+
+            if(selectedNode == null) return false;
+
+            selectedNode.Value = value;
+            return true;
+        }
+
+        // To add new node to linked list at selected position using index
+        public bool Insert(int index, T value)
+        {
+            if(index < 0 || index > Length - 1) return false;
+
+            if (index == 0)
+            {
+                UnShift(value);
+                return true;
+            }
+            else if(index == _length - 1)
+            {
+                Push(value);
+                return true;
+            }
+            else
+            {
+                var half = Math.Floor((decimal)_length / 2);
+
+                if (index < half)
+                {
+                    var current = _head;
+                    var prev = _head.Previous;
+                    var count = 0;
+
+                    while (count < index)
+                    {
+                        var temp = current;
+                        current = temp.Next;
+                        prev = current.Previous;
+
+                        count++;
+                    }
+
+                    var newNode = new DoubleLinkedListNode(value);
+
+                    prev.Next = newNode;
+                    newNode.Previous = prev;
+                    newNode.Next = current;
+                    current.Previous = newNode;
+
+                    _length++;
+                    return true;
+                }
+                else
+                {
+                    var current = _tail;
+                    var prev = _tail.Previous;
+                    var count = _length - 1;
+
+                    while (count > index)
+                    {
+                        var temp = current;
+                        current = temp.Previous;
+                        prev = current.Previous;
+
+                        count--;
+                    }
+
+                    var newNode = new DoubleLinkedListNode(value);
+
+                    prev.Next = newNode;
+                    newNode.Previous = prev;
+                    newNode.Next = current;
+                    current.Previous = newNode;
+
+                    _length++;
+                    return true;
+                }
+            }
+        }
+
+        // To remove the selected node
+        public bool Remove(int index)
+        {
+            if (index < 0 || index > Length - 1) return false;
+
+            if (index == 0)
+            {
+                Shift();
+                return true;
+            }
+            else if (index == _length - 1)
+            {
+                Pop();
+                return true;
+            }
+            else
+            {
+                var selectedNode = Get(index);
+
+                if (selectedNode == null) return false;
+
+                var prevNode = selectedNode.Previous;
+                var nextNode = selectedNode.Next;
+                prevNode.Next = nextNode;
+                nextNode.Previous = prevNode;
+
+                _length--;
+                return true;
+            }
+        }
+
+
+        // To display selected linked-list node value in string
+        public string DisplayNodeDetail(DoubleLinkedListNode node)
+        {
+            if (node == null) return "Node is Undefind or Null value";
+
+            var result = "";
+            try
+            {
+                var value = node.Value.ToString();
+                var prev = node.Previous == null ? "Null" : node.Previous.Value.ToString();
+                var next = node.Next == null ? "Null" : node.Next.Value.ToString();
+
+                result = "Node value is " + value
+                            + "\nPrevious Node value is " + prev
+                            + "\nNext Node value is " + next;
+
+                return result;
+            }
+            catch
+            {
+                result = "The value cannot display in string";
+                return result;
+            }
+        }
+
+        // To display all of linked-list node value in string
+        public string DisplayLinkedListValue()
+        {
+
+            if (_length == 0) return "Linked list is Empty";
+
+            var result = "";
+            try
+            {
+                var current = _head;
+
+                while (current != null)
+                {
+                    if (string.IsNullOrEmpty(result))
+                    {
+                        result = result + current.Value.ToString();
+                    }
+                    else
+                    {
+                        result = result + " <-> " + current.Value.ToString();
+                    }
+
+                    current = current.Next;
+                }
+
+                return result;
+            }
+            catch
+            {
+                result = "The value cannot display in string";
+                return result;
+            }
+        }
+
+
+        // Double linked list node class
+        public class DoubleLinkedListNode
+        {
+            public DoubleLinkedListNode(T Value)
+            {
+                this.Value = Value;
+                this.Next = null;
+                this.Previous = null;
+            }
+            public T Value { get; set; }
+            public DoubleLinkedListNode? Next { get; set; }
+            public DoubleLinkedListNode? Previous { get; set; }
+        }
     }
 
-    public class DoubleLinkedListNode<T>
-    {
-        public DoubleLinkedListNode(T Value)
-        {
-            this.Value = Value;
-            this.Next = null;
-        }
-        public T Value { get; set; }
-        public DoubleLinkedListNode<T>? Next { get; set; }
-    }
 }
--- DataStructurePlayGround.cs
+++ DataStructurePlayGround.cs
@@ -6,59 +6,296 @@ namespace Data_Structures
     {
         static void Main(string[] args)
         {
-            SinglyLinkedListTest();
+            //SinglyLinkedListTest();
+            DoubleLinkedListTest();
         }
 
         private static void SinglyLinkedListTest()
         {
             var LinkedList = new SinglyLinkedList<String>();
 
-            LinkedList.Push("Mike");
-            LinkedList.Push("Love");
-            LinkedList.Push("Ton");
+            // Wright test case here
 
-            Console.WriteLine(LinkedList.Length);
+        }
+
+        private static void DoubleLinkedListTest()
+        {
+            var LinkedList = new DoubleLinkedList<String>();
+
+            // Wright test method here
+
+            #region Test of Push() method
+
+            Console.WriteLine("Node count is " + LinkedList.Length);
             Console.WriteLine(LinkedList.DisplayLinkedListValue());
 
-            LinkedList.Insert(2, "And");
+            Console.WriteLine("\nExecute Push() method\n");
 
-            Console.WriteLine(LinkedList.Length);
+            LinkedList.Push("Red");
+
+            Console.WriteLine("Node count is " + LinkedList.Length);
             Console.WriteLine(LinkedList.DisplayLinkedListValue());
 
-            LinkedList.Insert(2, "Earth");
+            Console.WriteLine("\nExecute 3 Push() method\n");
 
-            Console.WriteLine(LinkedList.Length);
+            LinkedList.Push("Blue");
+            LinkedList.Push("Green");
+            LinkedList.Push("Yellow");
+
+            Console.WriteLine("Node count is " + LinkedList.Length);
             Console.WriteLine(LinkedList.DisplayLinkedListValue());
 
-            LinkedList.Remove(3);
+            Console.WriteLine("\n\n********** Next Section **********\n");
 
-            Console.WriteLine(LinkedList.Length);
+            #endregion
+
+            #region Test of Pop() method
+
+            Console.WriteLine("Node count is " + LinkedList.Length);
             Console.WriteLine(LinkedList.DisplayLinkedListValue());
 
-            LinkedList.Remove(3);
+            Console.WriteLine("\nExecute Pop() method\n");
 
-            Console.WriteLine(LinkedList.Length);
+            LinkedList.Pop();
+
+            Console.WriteLine("Node count is " + LinkedList.Length);
             Console.WriteLine(LinkedList.DisplayLinkedListValue());
 
-            LinkedList.Insert(2, "And");
+            Console.WriteLine("\nExecute 2 Pop() method\n");
 
-            Console.WriteLine(LinkedList.Length);
+            LinkedList.Pop();
+            LinkedList.Pop();
+
+            Console.WriteLine("Node count is " + LinkedList.Length);
             Console.WriteLine(LinkedList.DisplayLinkedListValue());
 
-            LinkedList.Insert(2, "Ton");
+            Console.WriteLine("\nExecute Pop() method\n");
 
-            Console.WriteLine(LinkedList.Length);
+            LinkedList.Pop();
+
+            Console.WriteLine("Node count is " + LinkedList.Length);
             Console.WriteLine(LinkedList.DisplayLinkedListValue());
-            Console.WriteLine("Head is " + LinkedList.Head.Value);
-            Console.WriteLine("Tail is " + LinkedList.Tail.Value);
 
-            Console.WriteLine("\nall Reverse\n");
+            Console.WriteLine("\n\n********** Next Section **********\n");
 
-            LinkedList.Reverse();
+            #endregion
 
+            #region Test of Shift() method
+            Console.WriteLine("Implement new Linked list value\n");
+
+            LinkedList = new DoubleLinkedList<String>();
+            LinkedList.Push("Gold");
+            LinkedList.Push("Silver");
+            LinkedList.Push("Crystal");
+
+            Console.WriteLine("Node count is " + LinkedList.Length);
             Console.WriteLine(LinkedList.DisplayLinkedListValue());
-            Console.WriteLine("Head is " + LinkedList.Head.Value);
-            Console.WriteLine("Tail is " + LinkedList.Tail.Value);
+
+            Console.WriteLine("\nExecute Shift() method\n");
+
+            LinkedList.Shift();
+
+            Console.WriteLine("Node count is " + LinkedList.Length);
+            Console.WriteLine(LinkedList.DisplayLinkedListValue());
+
+            Console.WriteLine("\nExecute Shift() method\n");
+
+            LinkedList.Shift();
+
+            Console.WriteLine("Node count is " + LinkedList.Length);
+            Console.WriteLine(LinkedList.DisplayLinkedListValue());
+
+            Console.WriteLine("\nExecute Shift() method\n");
+
+            LinkedList.Shift();
+
+            Console.WriteLine("Node count is " + LinkedList.Length);
+            Console.WriteLine(LinkedList.DisplayLinkedListValue());
+
+            Console.WriteLine("\n\n********** Next Section **********\n");
+            #endregion
+
+            #region Test of UnShift() method
+
+            Console.WriteLine("Node count is " + LinkedList.Length);
+            Console.WriteLine(LinkedList.DisplayLinkedListValue());
+
+            Console.WriteLine("\nExecute UnShift(\"Emerald\") method\n");
+
+            LinkedList.UnShift("Emerald");
+
+            Console.WriteLine("Node count is " + LinkedList.Length);
+            Console.WriteLine(LinkedList.DisplayLinkedListValue());
+
+            Console.WriteLine("\nExecute UnShift(\"Ruby\") method\n");
+
+            LinkedList.UnShift("Ruby");
+
+            Console.WriteLine("Node count is " + LinkedList.Length);
+            Console.WriteLine(LinkedList.DisplayLinkedListValue());
+
+            Console.WriteLine("\nExecute UnShift(\"Sapphire\") method\n");
+
+            LinkedList.UnShift("Sapphire");
+
+            Console.WriteLine("Node count is " + LinkedList.Length);
+            Console.WriteLine(LinkedList.DisplayLinkedListValue());
+
+            Console.WriteLine("\n\n********** Next Section **********\n");
+
+            #endregion
+
+            #region Test of Get() method
+
+            Console.WriteLine("Node count is " + LinkedList.Length);
+            Console.WriteLine(LinkedList.DisplayLinkedListValue());
+
+            Console.WriteLine("\nExecute Get() method for index 1\n");
+
+            var node = LinkedList.Get(1);
+            Console.WriteLine(LinkedList.DisplayNodeDetail(node));
+
+            Console.WriteLine("\nExecute Get() method for index 0\n");
+
+            node = LinkedList.Get(0);
+            Console.WriteLine(LinkedList.DisplayNodeDetail(node));
+
+            Console.WriteLine("\nExecute Get() method for index 99\n");
+
+            node = LinkedList.Get(99);
+            Console.WriteLine(LinkedList.DisplayNodeDetail(node));
+
+            Console.WriteLine("\nExecute 2 Push() method\n");
+
+            LinkedList.Push("Omega Ruby");
+            LinkedList.Push("Alpha Sapphire");
+
+            Console.WriteLine("Node count is " + LinkedList.Length);
+            Console.WriteLine(LinkedList.DisplayLinkedListValue());
+
+            Console.WriteLine("\nExecute Get() method for index 1\n");
+
+            node = LinkedList.Get(1);
+            Console.WriteLine(LinkedList.DisplayNodeDetail(node));
+
+            Console.WriteLine("\nExecute Get() method for index 3\n");
+
+            node = LinkedList.Get(3);
+            Console.WriteLine(LinkedList.DisplayNodeDetail(node));
+
+            Console.WriteLine("\n\n********** Next Section **********\n");
+
+            #endregion
+
+            #region Test of Set() method
+
+            Console.WriteLine("Node count is " + LinkedList.Length);
+            Console.WriteLine(LinkedList.DisplayLinkedListValue());
+
+            Console.WriteLine("\nExecute Set() method for index 0\n");
+
+            LinkedList.Set(0, "I love Pokemon");
+
+            Console.WriteLine("Node count is " + LinkedList.Length);
+            Console.WriteLine(LinkedList.DisplayLinkedListValue());
+
+            Console.WriteLine("\nExecute Set() method for index 3\n");
+
+            LinkedList.Set(3, "Nintendo Switch");
+
+            Console.WriteLine("Node count is " + LinkedList.Length);
+            Console.WriteLine(LinkedList.DisplayLinkedListValue());
+
+            Console.WriteLine("\n\n********** Next Section **********\n");
+
+            #endregion
+
+            #region Test of Insert() method
+
+            Console.WriteLine("Node count is " + LinkedList.Length);
+            Console.WriteLine(LinkedList.DisplayLinkedListValue());
+
+            Console.WriteLine("\nExecute Insert() method for index 3\n");
+
+            LinkedList.Insert(3, "Heart Gold");
+
+            Console.WriteLine("Node count is " + LinkedList.Length);
+            Console.WriteLine(LinkedList.DisplayLinkedListValue());
+
+            Console.WriteLine("\nExecute Insert() method for index 1\n");
+
+            LinkedList.Insert(1, "Yellow");
+
+            Console.WriteLine("Node count is " + LinkedList.Length);
+            Console.WriteLine(LinkedList.DisplayLinkedListValue());
+
+            Console.WriteLine("\nExecute Insert() method for last index\n");
+
+            LinkedList.Insert(LinkedList.Length - 1, "Very nuch ^_^");
+
+            Console.WriteLine("Node count is " + LinkedList.Length);
+            Console.WriteLine(LinkedList.DisplayLinkedListValue());
+
+            Console.WriteLine("\n\n********** Next Section **********\n");
+
+            #endregion
+
+            #region Test of Remove() method
+
+            Console.WriteLine("Node count is " + LinkedList.Length);
+            Console.WriteLine(LinkedList.DisplayLinkedListValue());
+
+            Console.WriteLine("\nExecute Remove() method for index 5\n");
+
+            LinkedList.Remove(5);
+
+            Console.WriteLine("Node count is " + LinkedList.Length);
+            Console.WriteLine(LinkedList.DisplayLinkedListValue());
+
+            Console.WriteLine("\nCheck connection at same index of removal\n");
+
+            node = LinkedList.Get(5);
+            Console.WriteLine(LinkedList.DisplayNodeDetail(node));
+
+            Console.WriteLine("\nExecute Remove() method for index 1\n");
+
+            LinkedList.Remove(1);
+
+            Console.WriteLine("Node count is " + LinkedList.Length);
+            Console.WriteLine(LinkedList.DisplayLinkedListValue());
+
+            Console.WriteLine("\nCheck connection at same index of removal\n");
+
+            node = LinkedList.Get(1);
+            Console.WriteLine(LinkedList.DisplayNodeDetail(node));
+
+            Console.WriteLine("\nExecute Remove() method for first index\n");
+
+            LinkedList.Remove(0);
+
+            Console.WriteLine("Node count is " + LinkedList.Length);
+            Console.WriteLine(LinkedList.DisplayLinkedListValue());
+
+            Console.WriteLine("\nCheck connection at same index of removal\n");
+
+            node = LinkedList.Get(0);
+            Console.WriteLine(LinkedList.DisplayNodeDetail(node));
+
+            Console.WriteLine("\nExecute Remove() method for last index\n");
+
+            LinkedList.Remove(LinkedList.Length - 1);
+
+            Console.WriteLine("Node count is " + LinkedList.Length);
+            Console.WriteLine(LinkedList.DisplayLinkedListValue());
+
+            Console.WriteLine("\nCheck connection at same index of removal\n");
+
+            node = LinkedList.Get(LinkedList.Length - 1);
+            Console.WriteLine(LinkedList.DisplayNodeDetail(node));
+
+            Console.WriteLine("\n\n********** Next Section **********\n");
+
+            #endregion
 
         }
     }
